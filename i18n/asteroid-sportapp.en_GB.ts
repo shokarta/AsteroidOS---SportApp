76fc60b071186b0bfbae20a25fa56210491bf162
shokarta <46573874+shokarta@users.noreply.github.com>
2019-02-04 16:41:02 +0100

    Add files via upload

--- i18n/asteroid-sportapp.en_GB.ts
+++ i18n/asteroid-sportapp.en_GB.ts
@@ -6,157 +6,157 @@
     <message id="sport-spp">
         <location filename="../main.qml" line="12"/>
         <source></source>
-        <oldsource>Sport App</oldsource>
-        <translation type="unfinished">Sport App</translation>
+        <oldsource>Sport Application</oldsource>
+        <translation>Sport Application</translation>
     </message>
     <message id="id-none">
         <location filename="../main.qml" line="63"/>
         <source></source>
         <oldsource>None</oldsource>
-        <translation type="unfinished">None</translation>
+        <translation>None</translation>
     </message>
     <message id="id-rowId">
         <location filename="../MainScreen.qml" line="56"/>
         <source></source>
         <oldsource>ID</oldsource>
-        <translation type="unfinished">ID</translation>
+        <translation>ID</translation>
     </message>
     <message id="id-rowGender">
         <location filename="../MainScreen.qml" line="64"/>
         <source></source>
         <oldsource>Gender</oldsource>
-        <translation type="unfinished">Gender</translation>
+        <translation>Gender</translation>
     </message>
     <message id="id-rowAge">
         <location filename="../MainScreen.qml" line="72"/>
         <source>Age</source>
-        <translation type="unfinished"></translation>
+        <translation>Age</translation>
     </message>
     <message id="id-rowAgeUnits">
         <location filename="../MainScreen.qml" line="72"/>
         <source>years</source>
-        <translation type="unfinished"></translation>
+        <translation>years</translation>
     </message>
     <message id="id-rowWeight">
         <location filename="../MainScreen.qml" line="80"/>
         <source>Weight</source>
-        <translation type="unfinished"></translation>
+        <translation>Weight</translation>
     </message>
     <message id="id-rowWorkoutsAmount">
         <location filename="../MainScreen.qml" line="124"/>
         <source>Total Workouts</source>
-        <translation type="unfinished"></translation>
+        <translation>Total Workouts</translation>
     </message>
     <message id="id-rowWorkoutsTimespent">
         <location filename="../MainScreen.qml" line="148"/>
         <source>Time Spent</source>
-        <translation type="unfinished"></translation>
+        <translation>Time Spent</translation>
     </message>
     <message id="id-rowWorkoutsDistance">
         <location filename="../MainScreen.qml" line="172"/>
         <source>Total Distance</source>
-        <translation type="unfinished"></translation>
+        <translation>Total Distance</translation>
     </message>
     <message id="id-rowWorkoutsCalories">
         <location filename="../MainScreen.qml" line="196"/>
         <source>Calories Burned</source>
-        <translation type="unfinished"></translation>
+        <translation>Calories Burned</translation>
     </message>
     <message id="id-rowWorkoutsFluid">
         <location filename="../MainScreen.qml" line="220"/>
         <source>Fluid Loss</source>
-        <translation type="unfinished"></translation>
+        <translation>Fluid Loss</translation>
     </message>
     <message id="id-startButton">
         <location filename="../MainScreen.qml" line="237"/>
         <source>NEW WORKOUT</source>
-        <translation type="unfinished"></translation>
+        <translation>NEW WORKOUT</translation>
     </message>
     <message id="id-lastWorkoutLabel">
         <location filename="../MainScreen.qml" line="261"/>
         <source>Last Workout</source>
-        <translation type="unfinished"></translation>
+        <translation>Last Workout</translation>
     </message>
     <message id="id-rowLastWorkoutAmount">
         <location filename="../MainScreen.qml" line="283"/>
         <source>ID Workout</source>
-        <translation type="unfinished"></translation>
+        <translation>ID Workout</translation>
     </message>
     <message id="id-rowLastWorkoutDate">
         <location filename="../MainScreen.qml" line="307"/>
         <source>Date</source>
-        <translation type="unfinished"></translation>
+        <translation>Date</translation>
     </message>
     <message id="id-rowLastWorkoutType">
         <location filename="../MainScreen.qml" line="331"/>
         <source>Sport Type</source>
-        <translation type="unfinished"></translation>
+        <translation>Sport Type</translation>
     </message>
     <message id="id-rowLastWorkoutTimespent">
         <location filename="../MainScreen.qml" line="355"/>
         <source>Time Spent</source>
-        <translation type="unfinished"></translation>
+        <translation>Time Spent</translation>
     </message>
     <message id="id-rowLastWorkoutDistance">
         <location filename="../MainScreen.qml" line="379"/>
         <source>Total Distance</source>
-        <translation type="unfinished"></translation>
+        <translation>Total Distance</translation>
     </message>
     <message id="id-rowLastWorkoutAvgSpeed">
         <location filename="../MainScreen.qml" line="403"/>
         <source>Average Speed</source>
-        <translation type="unfinished"></translation>
+        <translation>Average Speed</translation>
     </message>
     <message id="id-rowLastWorkoutCalories">
         <location filename="../MainScreen.qml" line="427"/>
         <source>Calories Burned</source>
-        <translation type="unfinished"></translation>
+        <translation>Calories Burned</translation>
     </message>
     <message id="id-rowLastWorkoutFluid">
         <location filename="../MainScreen.qml" line="451"/>
         <source>Fluid Loss</source>
-        <translation type="unfinished"></translation>
+        <translation>Fluid Loss</translation>
     </message>
     <message id="id-deleteLastButton">
         <location filename="../MainScreen.qml" line="479"/>
         <source>DELETE WORKOUT</source>
-        <translation type="unfinished"></translation>
+        <translation>DELETE WORKOUT</translation>
     </message>
     <message id="id-mainOngoingWorkoutSpeedUnit">
         <location filename="../OngoingWorkoutScreen.qml" line="83"/>
         <source>min/km</source>
-        <translation type="unfinished"></translation>
+        <translation>min/km</translation>
     </message>
     <message id="id-stopButtonLabelPause">
         <location filename="../OngoingWorkoutScreen.qml" line="176"/>
         <source>PAUSE</source>
-        <translation type="unfinished"></translation>
+        <translation>PAUSE</translation>
     </message>
     <message id="id-stopButtonLabelContinue">
         <location filename="../OngoingWorkoutScreen.qml" line="176"/>
         <source>CONTINUE</source>
-        <translation type="unfinished"></translation>
+        <translation>CONTINUE</translation>
     </message>
     <message id="id-label1">
         <location filename="../RegisterProfile1.qml" line="36"/>
         <source>Choose your gender</source>
-        <translation type="unfinished"></translation>
+        <translation>Choose your gender</translation>
     </message>
     <message id="id-noteLabel">
         <location filename="../RegisterProfile2.qml" line="36"/>
         <source>Set your birthdate</source>
-        <translation type="unfinished"></translation>
+        <translation>Set your birthdate</translation>
     </message>
     <message id="id-saveButton">
         <location filename="../RegisterProfile2.qml" line="85"/>
         <location filename="../RegisterProfile3.qml" line="52"/>
         <source>PROCEED</source>
-        <translation type="unfinished"></translation>
+        <translation>PROCEED</translation>
     </message>
     <message id="id-weightLabel">
         <location filename="../RegisterProfile3.qml" line="27"/>
         <source>Set your weight (in kg)</source>
-        <translation type="unfinished"></translation>
+        <translation>Set your weight (in kg)</translation>
     </message>
 </context>
 </TS>
